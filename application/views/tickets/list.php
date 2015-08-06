579cb29df6bf8b430bd030c2dc348a1fcf8d4368
Mikh <mikh.mail@gmail.com>
2015-08-06 15:59:35 +0300

    Переделал дизайн новой квитанции

--- application/views/tickets/list.php
+++ application/views/tickets/list.php
@@ -272,14 +272,14 @@ $row_global2 = array ();
 
         <table class="table table-bordered table-condensed">
 
-<?  foreach($kvitancys as $row) { ?>
+            <?  foreach($kvitancys as $row) { ?>
     <? $comments = $this->kvitancy_model->get_comments($row['id_kvitancy']); ?>
     <tr>
     <td>
         <a href="#" onclick="anichange(this); return false">
             <table class="table table-bordered table-condensed">
                 <tr>
-                    <td class="span2">
+                    <td class="span1">
                         <span style="background-color:<?=$row['background']?>" class="label">
                        # <?=$row['id_kvitancy']?>
                     </span>
@@ -294,7 +294,9 @@ $row_global2 = array ();
                             <span style="background-color:#b94a48" class="label">
                            <?echo floor((strtotime("now")-strtotime($row['date_priemka']))/86400) . ' дней';?>
                         </span>
-                        <?}
+                        <?}else{
+                            echo $row['date_vydachi'];
+                        }
                         ?>
                     </td>
                     <td class="span2"><?=$row['name_sc']?></td>
@@ -302,58 +304,22 @@ $row_global2 = array ();
                 </tr>
             </table>
         </a>
-    <div style="display: none;"> <!-- Only required for left/right tabs -->
+    <div style="display: none; margin-bottom: 20px;"> <!-- Only required for left/right tabs -->
         <ul class="nav nav-tabs">
             <li class="active">
                 <a href="#tab1_<?=$row['id_kvitancy']?>" data-toggle="tab">
 
-                    <span style="background-color:<?=$row['background']?>" class="label">
-                        <?=$row['name_sost']?>
-                    </span>
+                    Опции
                 </a></li>
-            <li><a href="#tab2_<?=$row['id_kvitancy']?>" data-toggle="tab">Опции</a></li>
+            <li><a href="#tab2_<?=$row['id_kvitancy']?>" data-toggle="tab">Информация</a></li>
             <li><a href="#tab3_<?=$row['id_kvitancy']?>" data-toggle="tab">Комментарии <sup class="badge"><?=count($comments)?></sup></a></li>
 
         </ul>
         <div class="tab-content">
             <div style="margin-bottom: 20px;" class="tab-pane active" id="tab1_<?=$row['id_kvitancy']?>">
 
-                <table class="table table-bordered table-condensed">
-                <tr>
 
-                    <td>
-                        <ul>
-                            
-                            <li><b>Адрес клиента:</b> <?=$row['adres']?></li>
-                            <li><b>Вид ремонта:</b>
 
-                            <?
-                            foreach ( $remont as $a=>$rowidrem)
-                            {
-                                if($rowidrem['id_remonta'] == $row['id_remonta']) echo $rowidrem['name_remonta'];
-                            }
-                            ?>
-                            <li><b>Внешний вид аппарата:</b> <?=$row['vid']?></li>
-                            <li><b>Серийный номер аппарата:</b> <?=$row['ser_nomer']?></li>
-                            <li><b>Комплектность:</b> <?=$row['komplektnost']?></li>
-                            <li><b>Приёмка:</b>
-
-                                <?
-                                foreach ($sc as $a=>$rowsc)
-                                {
-                                    if($rowsc['id_sc'] == $row['id_sc']) echo $rowsc['name_sc'];
-                                }
-                                ?>
-                            </li>
-                            <li><b>Примечания:</b> <?=$row['primechaniya']?></li>
-
-                        </ul>
-                    </td>
-                </tr>
-                </table>
-
-            </div>
-            <div style="margin-bottom: 20px;" class="tab-pane" id="tab2_<?=$row['id_kvitancy']?>">
                 <?=form_dropdown($row['id_kvitancy'], $options_sost, $row['id_sost'], 'id=status_' . $row['id_kvitancy'] . ' class=""')?>
 
                 <?if ($row['id_mechanic']) {
@@ -382,9 +348,48 @@ $row_global2 = array ();
                 <a href="<?=site_url()?>tickets/update/<?=$row['id_kvitancy']?>" class="btn btn-info" target="_blank">Редактировать</a>
                 <a href="<?=site_url()?>tickets/printing/<?=$row['id_kvitancy']?>" class="btn btn-danger" target="_blank">Печать</a>
                 <a href="<?=site_url()?>tickets/printing_check/<?=$row['id_kvitancy']?>" class="btn btn-danger" target="_blank">Печать чека</a>
+
+            </div>
+            <div style="margin-bottom: 20px;" class="tab-pane" id="tab2_<?=$row['id_kvitancy']?>">
+                <table class="table table-bordered table-condensed">
+                    <tr>
+
+                        <td>
+                            <ul>
+
+                                <li><b>Адрес клиента:</b> <?=$row['adres']?></li>
+                                <li><b>Вид ремонта:</b>
+
+                                    <?
+                                    foreach ( $remont as $a=>$rowidrem)
+                                    {
+                                        if($rowidrem['id_remonta'] == $row['id_remonta']) echo $rowidrem['name_remonta'];
+                                    }
+                                    ?>
+                                <li><b>Внешний вид аппарата:</b> <?=$row['vid']?></li>
+                                <li><b>Серийный номер аппарата:</b> <?=$row['ser_nomer']?></li>
+                                <li><b>Комплектность:</b> <?=$row['komplektnost']?></li>
+                                <li><b>Приёмка:</b>
+
+                                    <?
+                                    foreach ($sc as $a=>$rowsc)
+                                    {
+                                        if($rowsc['id_sc'] == $row['id_sc']) echo $rowsc['name_sc'];
+                                    }
+                                    ?>
+                                </li>
+                                <li><b>Примечания:</b> <?=$row['primechaniya']?></li>
+
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
             </div>
 
             <div style="margin-bottom: 20px;" class="tab-pane" id="tab3_<?=$row['id_kvitancy']?>">
+                <table class="table table-bordered table-condensed">
+                    <tr>
+                        <td>
                 <ul id="ul_<?=$row['id_kvitancy']?>">
                     <?foreach($comments as $rowc)
                     {?>
@@ -400,11 +405,12 @@ $row_global2 = array ();
                 </ul>
 
 
-                <input name="comment_<?=$row['id_kvitancy']?>"></input>
+                <textarea name="comment_<?=$row['id_kvitancy']?>"></textarea>
                 <input class="btn btn-info btn-mini" type="button" name="comment" id="comment_<?=$row['id_kvitancy']?>" value="Добавить комментарий"/>
 
-
-
+            </td>
+    </tr>
+</table>
 
             </div>
 
@@ -437,107 +443,129 @@ $row_global2 = array ();
 
     <!-- Модальное окно d-->
     <div id="modal_form">
-        <h1>Новая заявка</h1>
+        <h1>Новая заявка в ремонт</h1>
         <span id="modal_close">X</span>
 
         <form name="new_kvit_form" action="" method="post">
 
 
-            <div class="controls controls-row">
-                <input class="span2" type="text" id="inputApparat" onkeyup="look_apparat(this.value);" placeholder="Поиск аппарата">
-                <span> или </span>
-                <?php echo form_dropdown('id_aparat', $options_ap, $id_aparat_selected, 'class="" id="id_aparat" required="required" title="Аппарат"');?>
-                <span> или </span>
+            <div class="span12">
+                <div class="span6">
+                <input class="" type="text" id="inputApparat" onkeyup="look_apparat(this.value);" placeholder="Поиск аппарата">
 
-                <a href="#" class="btn" onclick="anichange(this); return false">Добавить</a>
+                <?php echo form_dropdown('id_aparat', $options_ap, $id_aparat_selected, 'class="" id="id_aparat" required="required" title="Аппарат"');?>
+
+
+
+                    <a href="#" class="btn" onclick="anichange(this); return false"><i class="icon-plus"></i></a>
 					<span name="aparat_span" style="display: none;">
-						<input class="span2" name="add_aparat_name" id="add_aparat_name" type="text" placeholder="Название аппарата">
+						<input class="" name="add_aparat_name" id="add_aparat_name" type="text" placeholder="Введите название аппарата">
 						<input class="btn" name="submit" id="add_aparat" type="button" value="Добавить аппарат" >
 					</span>
 
-                <div align="left" class="suggestionsBox" id="apparat_box" style="display: none;">
-                    <div class="suggestionList" id="apparat_list">
+                    <div align="left" class="suggestionsBox" id="apparat_box" style="display: none;">
+                        <div class="suggestionList" id="apparat_list">
+                        </div>
                     </div>
                 </div>
-            </div>
+
+                <div class="span6">
+
+                    <input class="" type="text" id="inputProizvod" onkeyup="look_proizvod(this.value);" placeholder="Поиск бренда">
+
+                    <?php echo form_dropdown('id_proizvod', $options_proizvoditel, $id_proizvod_selected, 'class="" id="id_proizvod" required="required" title="Бренд"');?>
 
 
-            <div class="controls controls-row">
-                <input class="span2" type="text" id="inputProizvod" onkeyup="look_proizvod(this.value);" placeholder="Поиск бренда">
-                <span>или</span>
-                <?php echo form_dropdown('id_proizvod', $options_proizvoditel, $id_proizvod_selected, 'class="" id="id_proizvod" required="required" title="Бренд"');?>
-                <span>или</span>
-
-                <a href="#" class="btn" onclick="anichange(this); return false">Добавить</a>
+                    <a href="#" class="btn" onclick="anichange(this); return false"><i class="icon-plus"></i></a>
 					<span name="proizvod_span" style="display: none;">
-						<input class="span2" name="add_proizvod_name" id="add_proizvod_name" type="text" placeholder="Название бренда">
+						<input class="" name="add_proizvod_name" id="add_proizvod_name" type="text" placeholder="Введите название бренда">
 						<input class="btn" name="submit" id="add_proizvod" type="button" value="Добавить бренд">
 					</span>
 
-                <div class="suggestionsBox" id="proizvod_box" style="display: none;">
-                    <div class="suggestionList" id="proizvod_list">
+                    <div class="suggestionsBox" id="proizvod_box" style="display: none;">
+                        <div class="suggestionList" id="proizvod_list">
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+            <div class="span12">
+                </div>
+
+
+
+            <div class="span12">
+                <div class="span4">
+                <input autocomplete="off" name="model" type="text" placeholder="Введите модель аппарата" required="required" title="Модель">
+                    </div>
+                <div class="span4">
+                <input autocomplete="off" name="ser_nomer" type="text" placeholder="Введите серийный номер" required="required" title="Серийный номер">
+                </div>
+            </div>
+
+            <div class="span12">
+            <div class="span4">
+                <textarea id="" rows="3" name="neispravnost" placeholder="Введите неисправность, например: 'Не включается'" required="required" title="Неисправность"></textarea>
+            </div>
+
+            <div class="span4">
+                <textarea id="" rows="3" name="komplektnost" placeholder="">Без упаковки (без заводского комплекта), без блока питания, без сетевых (соединительных) кабелей, без SIM карт и съемных носителей.</textarea>
+            </div>
+
+            <div class="span4">
+                <textarea id="" rows="3" name="vid" placeholder="appearance">Внешний вид: следы эксплуатации: царапины, потёртости, б/у </textarea>
+            </div>
+                </div>
+
+            <div class="span12">
+            </div>
+
+            <div class="span12">
+            <div class="span4">
+
+                <?php  echo form_dropdown('id_remonta', $options_id_remonta, $id_remonta_selected, 'class="" id="id_remonta" required="required" title="Тип ремонта"');?>
+            </div>
+            <div class="span4">
+
+                <?php  echo form_dropdown('id_sc', $options_id_sc, '', 'class="" id="id_sc" required="required" title="Сервисный Центр"');?>
+            </div>
+            <div class="span4">
+
+                    <textarea name="primechaniya" placeholder="Если есть что добавить - напишите примечание"></textarea>
+            </div>
+            </div>
+
+
+
+            <div class="span12">
+            <input autocomplete="off" name="search_user" id="search_user" type="search" placeholder="Поиск клиентов по фамилии">
+                <div id="user_box" style="display: none;">
+                    <div id="user_list">
                     </div>
                 </div>
             </div>
 
 
-
-            <br>
-
-
             <div class="span12">
-                <input autocomplete="off" name="model" type="text" placeholder="Введите модель аппарата" required="required" title="Модель">
-                <input autocomplete="off" name="ser_nomer" type="text" placeholder="Введите серийный номер" required="required" title="Серийный номер">
-            </div>
-            <div class="span3">
-                <textarea id="" rows="3" name="neispravnost" placeholder="Введите неисправность, например: 'Не включается'" required="required" title="Неисправность"></textarea>
-            </div>
-
-            <div class="span3">
-                <textarea id="" rows="3" name="komplektnost" placeholder="">
-                    Без упаковки (без заводского комплекта), без блока питания, без сетевых (соединительных) кабелей, без SIM карт и съемных носителей.
-                </textarea>
-            </div>
-
-            <div class="span3">
-                <textarea id="" rows="3" name="vid" placeholder="appearance">Внешний вид: следы эксплуатации: царапины, потёртости, б/у
-                </textarea>
-            </div>
-            <div class="span4">
-                <label for="id_remonta">Вид ремонта</label>
-                <?php  echo form_dropdown('id_remonta', $options_id_remonta, $id_remonta_selected, 'class="" id="id_remonta" title="Тип ремонта"');?>
-            </div>
-            <div class="span4">
-                <label for="id_sc">Сервисный Центр</label>
-                <?php  echo form_dropdown('id_sc', $options_id_sc, '', 'class="" id="id_sc" required="required" title="Сервисный Центр"');?>
-            </div>
-
-            <div class="span12">
-                <textarea id="" rows="2" name="primechaniya" placeholder="Примечание"></textarea>
-            </div>
-
-            <hr>
-            <input autocomplete="off" name="search_user" id="search_user" type="search" placeholder="Поиск клиентов по фамилии">
-
-            <div id="user_box" style="display: none;">
-                <div id="user_list">
-                </div>
-            </div>
-
-            <br>
             <input name="user_id" id="user_id" type="hidden">
             <input autocomplete="off" name="fam" id="fam" type="text" placeholder="Фамилия" required="required" title="Фамилия">
             <input autocomplete="off" name="imya" id="imya" type="text" placeholder="Имя" required="required" title="Имя">
             <input autocomplete="off" name="otch" id="otch" type="text" placeholder="Отчество" title="Отчество">
-            <br>
+
             <input autocomplete="off" name="phone" id="phone" type="text" placeholder="Телефон" required="required" title="Телефон">
             <input autocomplete="off" name="mail" id="mail" type="text" placeholder="E-MAIL">
             <input autocomplete="off" name="adres" name="adres" type="text" placeholder="Адрес">
-            <br>
+            </div>
 
 
-
+            <div class="span12">
             <input type="button" name="new_kvit" value="Добавить" class="btn btn-primary">
+                </div>
         </form>
+
+
+
     </div>
     <div id="overlay"></div>
