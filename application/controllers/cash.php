31aa34fbd70e4506418415eb5c26890efa96a802
Mikh <mikh.mail@gmail.com>
2017-08-01 11:06:30 +0300

    Исправил ошибку в навигации в кассе

--- application/controllers/cash.php
+++ application/controllers/cash.php
@@ -37,7 +37,8 @@ class Cash extends CI_Controller {
         $config['per_page'] = 10;
         $config['base_url'] = base_url() . 'cash/';
         $config['use_page_numbers'] = TRUE;
-        $config['first_url'] = '';
+        $config['display_pages'] = TRUE;
+        $config['first_url'] = '1';
         $config['full_tag_open'] = '<ul>';
         $config['full_tag_close'] = '</ul>';
         $config['num_tag_open'] = '<li>';
